d2d8fcb7033c8578621de88b85da4eed428b5529
Metaferya <fitse07@gmail.com>
2024-12-05 11:53:50 -0600

    HOMEWORK 9

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,58 +1,40 @@
-CREATE TABLE titles(
-	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	PRIMARY KEY (title_id)
-);
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
 
-SELECT * FROM titles;
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
-CREATE TABLE employees(
-	emp_no INT NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-	PRIMARY KEY (emp_no)
-);
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN employees e ON dm.emp_no = e.emp_no
+JOIN departments d ON dm.dept_no = d.dept_no;
 
-SELECT * FROM employees;
+SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
 
-CREATE TABLE departments(
-	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR NOT NULL,
-	PRIMARY KEY (dept_no)
-);
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
-SELECT * FROM departments;
+SELECT e.emp_no, e.last_name, e.first_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
 
-CREATE TABLE dept_manager(
-	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY (dept_no, emp_no)
-);
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
 
-SELECT * FROM dept_manager;
+SELECT last_name, COUNT(*) as frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
 
-CREATE TABLE dept_emp(
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
 
-SELECT * FROM dept_emp;
-
-CREATE TABLE salaries(
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (emp_no)
-);
-
-SELECT * FROM salaries;
